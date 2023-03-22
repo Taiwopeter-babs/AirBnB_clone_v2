6b2bb61bb611d81246cb121bd27e49fa3923dddc
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-22 23:12:49 +0100

    enable parsing of string class name

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,6 +13,14 @@ class FileStorage:
         """Returns a dictionary of models of specified class
         currently in storage
         """
+        from models.base_model import Base
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
+
         if cls:
             cls_dict = {}
             if isinstance(cls, str):
